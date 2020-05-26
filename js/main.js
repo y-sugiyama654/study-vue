79e799d745641028fa3340276dacd47b166fa14c
Yuta <y.sugiyama54@gmail.com>
2020-05-27 01:09:17 +0900

    タスクを追加して配列に格納

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ new Vue({
     el: '#app',
     data: {
         newItem: '',
+        todos: [],
     },
     methods: {
         addItem: function(event) {
-            alert();
+            var todo = {
+                item: this.newItem,
+            };
+            this.todos.push(todo);
         }
     }
 })
