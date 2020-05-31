ad93d50ab61619303bab397b304bfc4694f6cccc
Yuta <y.sugiyama54@gmail.com>
2020-05-31 17:55:07 +0900

    bitcoin価格をapiから取得してブラウザに表示

--- bitcoinPrice/js/main.js
+++ bitcoinPrice/js/main.js
@@ -1,3 +1,15 @@
 new Vue({
     el: '#app',
+    data: {
+        info: null,
+    },
+    mounted: function () {
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(function(response){
+                this.info = response.data.bpi;
+            }.bind(this))
+            .catch(function(error){
+                console.log(error);
+            })
+    },
 })
