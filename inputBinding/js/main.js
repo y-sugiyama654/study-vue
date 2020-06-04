87383af7d8b152de6e0b920dff95bca312a54c0a
Yuta <y.sugiyama54@gmail.com>
2020-06-04 22:51:29 +0900

    入力フォームバインディング

---
+++ inputBinding/js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        counter: 0,
+        message: '',
+        checked: false,
+        colors: [],
+        color: '',
+        selected: '',
+        multiple: [],
+        age: 0,
+    },
+})
