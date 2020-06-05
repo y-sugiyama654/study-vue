819934b3bd9c3f99a552ec59e0ac9ae9bb0d5c47
Yuta <y.sugiyama54@gmail.com>
2020-06-05 22:52:24 +0900

    単一ファイルコンポーネントの作成

---
+++ cli_project/src/components/HelloNuxt.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Hello Nuxt</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HelloNuxt.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
